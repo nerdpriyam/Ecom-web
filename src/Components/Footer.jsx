3fa7ca7b6e89360ccf5ed2223feda03b876b83fa
priyam srivastava <er.priyamsrivastava@gmail.com>
2026-04-11 16:20:41 +0530

    add footer and navbar 11-april-2026

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,197 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>
-      This is the Footer
+   <footer className="pt-lg-13 bg-light py-8">
+    <div className="container">
+      <div className="row gy-8">
+        <div className="col-md-4">
+          <div className="">
+            <a className="d-inline-flex gap-2 align-items-center lh-1" href="index.html">
+              <span className="text-primary"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28"
+                  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                  stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-book">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                  <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                  <path d="M3 6l0 13" />
+                  <path d="M12 6l0 13" />
+                  <path d="M21 6l0 13" />
+                </svg></span>
+              <span className="fw-bold">LearnHub</span>
+            </a>
+            <p className="mt-4 mb-6">Empowering millions of learners worldwide with industry-leading courses and expert
+              mentors.</p>
+            <div className="d-flex flex-column gap-2">
+              <span className="d-flex align-items-center gap-2">
+                <span className="text-primary">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    className="icon icon-tabler icons-tabler-outline icon-tabler-mail">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+                    <path d="M3 7l9 6l9 -6" />
+                  </svg>
+
+                </span>
+                <span>hello@learnhub.com</span>
+
+              </span>
+              <span className="d-flex align-items-center gap-2">
+                <span className="text-primary">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    className="icon icon-tabler icons-tabler-outline icon-tabler-phone">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path
+                      d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2" />
+                  </svg>
+
+                </span>
+                <span>+1 (234) 567-890</span>
+              </span>
+              <span className="d-flex align-items-center gap-2">
+                <span className="text-primary">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    className="icon icon-tabler icons-tabler-outline icon-tabler-map-pin">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                    <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+                  </svg>
+
+                </span>
+                <span>San Francisco, CA</span>
+              </span>
+            </div>
+
+
+
+
+
+
+          </div>
+
+        </div>
+        <div className="col-md-8">
+          <div className="row">
+            <div className="col-lg-3 col-md-6">
+              <div className="">
+                <h4 className="fs-5 mb-4">Product</h4>
+                <ul className="list-unstyled lh-lg small">
+                  <li><a href="#">Features</a>
+
+
+                  </li>
+                  <li><a href="#">Pricing</a></li>
+                  <li><a href="#">Courses</a></li>
+                  <li><a href="#">Community</a></li>
+                </ul>
+              </div>
+
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <div>
+                <h4 className="fs-5 mb-4">Company</h4>
+                <ul className="list-unstyled lh-lg small">
+                  <li><a href="#">About Us</a>
+
+
+                  </li>
+                  <li><a href="#">Careers</a></li>
+                  <li><a href="#">Blog</a></li>
+                  <li><a href="#">Press</a></li>
+                </ul>
+              </div>
+
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <div>
+                <h4 className="fs-5 mb-4">Support</h4>
+                <ul className="list-unstyled lh-lg small">
+                  <li><a href="#">Help Center</a>
+
+
+                  </li>
+                  <li><a href="#">Contact Us</a></li>
+                  <li><a href="#">Privacy Policy</a></li>
+                  <li><a href="#">Terms of Service</a></li>
+                </ul>
+              </div>
+
+            </div>
+            <div className="col-lg-3 col-md-6">
+              <div>
+                <h4 className="fs-5 mb-4">Help Center</h4>
+                <ul className="list-unstyled lh-lg small">
+                  <li><a href="#">Web Development</a>
+
+
+                  </li>
+                  <li><a href="#">UI/UX Design</a></li>
+                  <li><a href="#">Data Science</a></li>
+                  <li><a href="#">Marketing</a></li>
+                </ul>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+      <div className="border-top mt-8 pt-6 row small">
+        <div className="col-12 d-flex flex-column flex-md-row justify-content-between">
+          <div>
+            <p>© 2026 LearnHub. All rights reserved. Developed by <a href="https://codescandy.com/" className="link-primary" target="_blank">Codescandy</a> • Distributed by <a href="https://themewagon.com/" className="link-primary" target="_blank">ThemeWagon</a>
+            </p>
+          </div>
+          <div>
+            <div className="">
+
+              <a href="#" className="btn rounded-circle btn-light btn-icon btn-xs">
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none"
+                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-brand-x">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M4 4l11.733 16h4.267l-11.733 -16z"></path>
+                  <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772"></path>
+                </svg>
+
+              </a>
+              <a href="#" className="btn rounded-circle btn-light btn-icon btn-xs">
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none"
+                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                  className="icon icon-tabler icons-tabler-outline icon-tabler-brand-linkedin ">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M8 11v5"></path>
+                  <path d="M8 8v.01"></path>
+                  <path d="M12 16v-5"></path>
+                  <path d="M16 16v-3a2 2 0 1 0 -4 0"></path>
+                  <path d="M3 7a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4z"></path>
+                </svg>
+
+              </a>
+              <a href="#" className="btn rounded-circle btn-light btn-icon btn-xs">
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none"
+                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                  className="icon icon-tabler icons-tabler-brand-github ">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path
+                    d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12 12 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5">
+                  </path>
+                </svg>
+
+              </a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
+  </footer>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>
-     This is the Navbar
-     <h1>welcome to navbar</h1>
-    </div>
+    <nav className="navbar navbar-expand-lg bg-white shadow-sm sticky-top">
+      <div className="container">
+        <a className="navbar-brand d-inline-flex gap-2 align-items-center lh-1" href="index.html">
+          <span className="text-primary"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"
+            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-book">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+            <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+            <path d="M3 6l0 13" />
+            <path d="M12 6l0 13" />
+            <path d="M21 6l0 13" />
+          </svg></span>
+          <span className="fw-bold">LearnHub</span>
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li className="nav-item"><a href="#hero" className="nav-link">Home</a></li>
+            <li className="nav-item"><a href="#courses" className="nav-link">Courses</a></li>
+            <li className="nav-item"><a href="#mentor" className="nav-link">Mentor</a></li>
+            <li className="nav-item"><a href="#group" className="nav-link">Group</a></li>
+            <li className="nav-item"><a href="#testimonials" className="nav-link">Testimonials</a></li>
+            <li className="nav-item"><a href="#pricing" className="nav-link">Pricing</a></li>
+          </ul>
+          <div className="d-flex gap-3 align-items-center">
+            <a href="#" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Get Started</a>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
